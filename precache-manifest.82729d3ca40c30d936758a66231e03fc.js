9d201f0ced647fb489a8e47f0f1feb33c6230e02
Meguru Yasuda <meguruyasuda@D-10-19-246-251.dhcp4.washington.edu>
2019-07-10 14:42:52 -0700

    Updates

---
+++ precache-manifest.82729d3ca40c30d936758a66231e03fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bfcd38cff045debae7c88239e31d6f4",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "cb5eef86bf622273a44b",
+    "url": "/my-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0955c9d205ad4332b268",
+    "url": "/my-app/static/js/2.8ed13e2a.chunk.js"
+  },
+  {
+    "revision": "cb5eef86bf622273a44b",
+    "url": "/my-app/static/js/main.231da08f.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
